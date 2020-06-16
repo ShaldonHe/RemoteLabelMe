40ccbda318734e135cdfaf61b4ef8b7e7fa6da89
Sheldon.He <seariver@foxmail.com>
2020-06-16 12:06:01 +0800

    .

--- labelme/server.py
+++ labelme/server.py
@@ -1,23 +1,29 @@
 import requests
-server_url = 'http://127.0.0.1:5000/'
+server_url = 'http://127.0.0.1:5000'
 def projec_list():
     res = requests.get(server_url+'projects')
     return res.json()
 
-def datasets(project_id):
-    res = requests.get(server_url+'datasets/'+project_id)
+def project(project_id):
+    url = f'{server_url}/projects/{project_id}'
+    res = requests.get(url)
+    return res.json()
+
+def dataset(project_id):
+    url = f'{server_url}/dataset/{project_id}'
+    res = requests.get(url)
     return res.json()
 
 def image(image_id):
-    res = requests.get(server_url+'image/'+image_id)
+    url = f'{server_url}/image/{image_id}'
+    res = requests.get(url)
     return res.json()
 
 def label(project_id,dataset_id,image_id, data = None):
+    url = f'{server_url}/label/{project_id}/{dataset_id}/{image_id}'
     if data is None:
-        url = f'{server_url}label/{project_id}/{dataset_id}/{image_id}'
         res = requests.get(url)
         return res.json()
     else:
-        url = f'{server_url}label/{project_id}/{dataset_id}/{image_id}'
         res = requests.post(url,data)
         return res.json()
